38ab8866435bd82bc22a3664d7475f6500930b50
alexismtaylor <alexismtaylor2@gmail.com>
2017-04-03 20:10:33 -0400

    Update views.py

--- python_proj/app/views.py
+++ python_proj/app/views.py
@@ -1,26 +1,13 @@
 from flask import render_template, request, flash
 from app import app
 from app.forms import Drop
-#from app.factbook_scraper import get_info
+from app.recipescraper import getFood
 
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST' and request.form['submit1'] == 'submitted':
         select = request.form.get('maindish')
         select2 = request.form.get('sidedish')
         select3 = request.form.get('vegetable')
-        print(select + " " + select2 + " " + select3)
+        str1 = getFood(select, select2, select3) #get url or string
     return render_template('index.html')
-
-
-'''
-@app.route('/search', methods=['GET', 'POST'])
-def search():
-    info = None
-    form = Drop()
-    if form.validate_on_submit():
-        country = form.main.data
-        #info = get_info(country)
-    return render_template('search.html', title='Search for a country', form=form, info=info)
-'''
