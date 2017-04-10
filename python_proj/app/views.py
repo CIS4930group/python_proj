dda67fe8638e2409d1b0b38d6222da9d6813f075
alexismtaylor <alexismtaylor2@gmail.com>
2017-04-10 13:27:14 -0400

    Updated views.py

--- python_proj/app/views.py
+++ python_proj/app/views.py
@@ -2,12 +2,18 @@ from flask import render_template, request, flash
 from app import app
 from app.forms import Drop
 from app.recipescraper import getFood
+import requests, re
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    info=[]
     if request.method == 'POST' and request.form['submit1'] == 'submitted':
         select = request.form.get('maindish')
         select2 = request.form.get('sidedish')
         select3 = request.form.get('vegetable')
-        str1 = getFood(select, select2, select3) #get url or string
-    return render_template('index.html')
+        temps = getFood(select, select2, select3) #get url or string
+        info.append(temps)
+        temp = temps.rsplit('/', 2)[-2]
+        temp2 = temp.replace("-", " ")
+        info.append(temp2.title())
+    return render_template('index.html', info=info)
