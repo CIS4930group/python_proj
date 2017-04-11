389850db1735336646a5dd6f9623ce933139fb58
alexismtaylor <alexismtaylor2@gmail.com>
2017-04-11 14:01:47 -0400

    Updated views.py

--- python_proj/app/views.py
+++ python_proj/app/views.py
@@ -1,19 +1,23 @@
 from flask import render_template, request, flash
 from app import app
-from app.forms import Drop
 from app.recipescraper import getFood
-import requests, re
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     info=[]
-    if request.method == 'POST' and request.form['submit1'] == 'submitted':
-        select = request.form.get('maindish')
-        select2 = request.form.get('sidedish')
-        select3 = request.form.get('vegetable')
-        temps = getFood(select, select2, select3) #get url or string
-        info.append(temps)
-        temp = temps.rsplit('/', 2)[-2]
-        temp2 = temp.replace("-", " ")
-        info.append(temp2.title())
+    if request.method == 'POST':
+        if request.form['submit1'] == 'submitted':
+            select = request.form.get('maindish')
+            select2 = request.form.get('sidedish')
+            select3 = request.form.get('vegetable')
+            if not select or not select2 or not select3:    #make sure the user actually selected something
+                info=[]
+            else:
+                temps = getFood(select, select2, select3)   #get url or string
+                info.append(temps)
+                temp = temps.rsplit('/', 2)[-2]             #get name of recipe
+                temp2 = temp.replace("-", " ")
+                info.append(temp2.title())                  #make recipe name a title
+        elif request.form['random'] == 'action':
+            print("Pressed this")
     return render_template('index.html', info=info)
