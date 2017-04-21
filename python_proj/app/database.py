fa2964edaa02661a9420538d26632dc85c2632fc
alexismtaylor <alexismtaylor2@gmail.com>
2017-04-21 12:09:51 -0400

    Update database.py

--- python_proj/app/database.py
+++ python_proj/app/database.py
@@ -4,16 +4,19 @@ def storeUsers(user, passw):
 	#tries to add to the DB if it exists
 	try:
 		s = shelve.open('users.db', writeback = True)
-		if user in s['key1']: #replace matt with username
+		if user in s: #replace matt with username
 			s.close()
 			return "failed"
 		else:
-			s['key1'][user] = passw #user and pass
+			s[user]['password'] = passw
+			#s['key1']['loggedIn'] = False
 
 	#creates the db if it does not already exist
 	except KeyError:
+		favList = []
 		s = shelve.open('users.db')
-		s['key1'] = {user : passw}
+		s[user] = {'password': passw, 'favorites': favList}
 
 
 	s.close()
+
